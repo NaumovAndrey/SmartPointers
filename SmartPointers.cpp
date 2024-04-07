b89cf873904af409f5d0c858c486d394d18b0454
andrey <naumov.a.n@icloud.com>
2024-04-07 21:20:30 +0700

    Добавьте файлы проекта.

---
+++ SmartPointers.cpp
@@ -0,0 +1,12 @@
+﻿// SmartPointers.cpp: определяет точку входа для приложения.
+//
+
+#include "SmartPointers.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
