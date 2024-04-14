564c9c7d9b43da4c637bfd0cadda221589eb9c44
andrey <naumov.a.n@icloud.com>
2024-04-14 11:24:05 +0700

    умные указатели

--- SmartPointers.cpp
+++ SmartPointers.cpp
@@ -18,61 +18,64 @@ class Toy
 	std::string name;
 };
 
+class smart_ptr_toy
+{
+private:
+    Toy* obj;
+public:
+    smart_ptr_toy()
+    {
+		obj = new Toy("SomeName");
+    }
+
+	smart_ptr_toy(std::string name)
+	{
+		obj = new Toy(name);
+	}
+
+	smart_ptr_toy(const smart_ptr_toy& oth) // конструктор копирования
+	{
+		obj = new Toy(*oth.obj);
+	}
+
+	smart_ptr_toy& operator = (const smart_ptr_toy& oth) //оператор присванивания
+	{
+		if(this = &oth) return *this;
+		if(obj != nullptr) delete obj;
+		obj = new Toy(*oth.obj);
+
+		return *this;
+	}
+
+	~smart_ptr_toy() { delete obj; }; //деструктор
+};
 
 class Dog 
 {
 public:
-	Dog(string _name, std::string toyname, int _age) : name(_name)
+	Dog(string _name, std::string toyname, int _age) : name(_name), lovelyToy(toyname)
 	{
-		//name = _name;
 		if (_age >= 0 && _age < 30) age = _age;
-		lovelyToy = new Toy(toyname);
 	}
 
 	Dog() : Dog("Unknown", "UnknownToy", 0) {};
 	Dog(string _name) : Dog(_name, "UnknownToy", 0) {};
 	Dog(int _age) : Dog("Unknown", "UnknownToy", _age) {};
 
-	Dog(const Dog& other) //Конструктор копирования
-	{
-		name = other.name;
-		age = other.age;
-		lovelyToy = new Toy(*other.lovelyToy);
-	}
-
-	Dog& operator = (const Dog& other) //оператор присваивания
-	{
-		if (this == &other) return *this;
-		name = other.name;
-		age = other.age;
-
-		if (lovelyToy != nullptr) delete lovelyToy;
-		lovelyToy = new Toy(*other.lovelyToy);
-
-		return *this;
-	}
-
-	virtual ~Dog()
-	{
-		delete lovelyToy;
-		cout << "Диструктор: удаляю динамическую переменную класса игрушка" << endl;
-	}
-
+	
 private:
 	string name;
 	int age;
-	Toy* lovelyToy;
-	
-	
+	smart_ptr_toy lovelyToy;
 };
 
+void foo(std::unique_ptr<Dog> d) {};
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	
-	Dog d("drug", "Ball", 1);
-	Dog q(d);
+	std::unique_ptr<Dog> d = std::make_unique<Dog>("Шарик", "Мяч", 1); //один динамический объект - один владелец
 
 	return 0;
 }
